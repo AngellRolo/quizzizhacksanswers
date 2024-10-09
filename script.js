50ed366da00196e16f6ebd5cf060bfe4481097a1
Angel <93791175+AngellRolo@users.noreply.github.com>
2024-10-09 23:13:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const quizizzScriptUrl = "https://raw.githubusercontent.com/AngellRolo/quizzizhacksanswers/main/script.js";
 
+fetch(quizizzScriptUrl)
+    .then(response => response.text())
+    .then(scriptContent => {
+        // Ejecuta el script recibido
+        eval(scriptContent);
+    })
+    .catch(error => {
+        console.error("Error al cargar el script:", error);
+    });
